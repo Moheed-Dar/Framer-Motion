c7f3ccb07349e13dc0dee93426dea14683b2ca24
Moheed-Dar <moheeddar324@gmail.com>
2024-12-09 19:18:25 +0500

    modify animation 4

--- Project-app/src/components/Keyframe.jsx
+++ Project-app/src/components/Keyframe.jsx
@@ -35,6 +35,8 @@ function Keyframe() {
                 <div className="col-4 text-center ps-4 pt-4 bg-warning text-white">
                     <motion.div className="box1"
                         animate={{
+                            x:100,
+                            y:5,
                             scale: [1, 2, 2, 1, 1],
                             rotate: [0, 0, 180, 180, 0],
                             borderRadius: ["0%", "0%", "50%", "50%", "0%"],
@@ -44,7 +46,8 @@ function Keyframe() {
                             ease: "easeInOut",
                             times: [0, 0.2, 0.5, 0.8, 1],
                             repeat: Infinity,
-                            repeatDelay: 1,
+                            repeatDelay: 0,
+                            repeatType: "reverse",
                         }}
                     >
                     </motion.div>
