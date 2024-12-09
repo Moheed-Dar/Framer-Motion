376953bed77682adfdc20c511953039f191540c3
Moheed-Dar <moheeddar324@gmail.com>
2024-12-09 19:10:34 +0500

    animation 4

--- Project-app/src/components/Keyframe.jsx
+++ Project-app/src/components/Keyframe.jsx
@@ -8,28 +8,46 @@ function Keyframe() {
             <div className="row">
                 <div className="col-4 text-center pt-3 bg-gray-600 text-white">
                     <motion.div className="box" animate={{
-                        x: 100,
-
-                        transition: { duration: 3, }
-                    }}>
-
+                        x: 300,
+                    }}
+                    transition={{
+                        duration:1,
+                        repeat: Infinity,
+                        repeatDelay: 1,
+                    }}
+                    >
                     </motion.div>
                 </div>
                 <div className="col-4 text-center p-3 bg-red-700 text-white">
-                    <motion.div className="box" 
-                    initial={{ opacity: 0, scale: 0.5 }}
-                    animate={{ opacity: 4, scale: 1 }}
-                    transition={{
-                      duration: 2,
-                      delay: 0.5,
-                      ease: [0, 0.71, 0.2, 1.01]
-                    }}
-                     >
-
+                    <motion.div className="box"
+                        initial={{ opacity: 0, scale: 0.5 }}
+                        animate={{ opacity: 4, scale: 1 }}
+                        transition={{
+                            duration: 2,
+                            delay: 0.5,
+                            ease: [0, 0.71, 0.2, 1.01],
+                            repeat: Infinity,
+                            repeatDelay: 1,
+                        }}
+                    >
                     </motion.div>
                 </div>
-                <div className="col-4 text-center p-3 bg-warning text-white">
-
+                <div className="col-4 text-center ps-4 pt-4 bg-warning text-white">
+                    <motion.div className="box1"
+                        animate={{
+                            scale: [1, 2, 2, 1, 1],
+                            rotate: [0, 0, 180, 180, 0],
+                            borderRadius: ["0%", "0%", "50%", "50%", "0%"],
+                        }}
+                        transition={{
+                            duration: 2,
+                            ease: "easeInOut",
+                            times: [0, 0.2, 0.5, 0.8, 1],
+                            repeat: Infinity,
+                            repeatDelay: 1,
+                        }}
+                    >
+                    </motion.div>
                 </div>
             </div>
         </div>
